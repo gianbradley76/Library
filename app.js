561aec6bbe510426ced8a38913550b6c9dea36a3
Gian Bradley Mendoza <gbpmendoza76@gmail.com>
2021-03-15 20:59:34 +0800

    Refactored the progeram

--- app.js
+++ app.js
@@ -1,90 +1,156 @@
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pubYear = document.getElementById("pubYear");
-const status = document.getElementById("status");
-const addBook = document.getElementById("submit");
-const bookList = document.querySelector(".book-list");
-
-let myLibrary = [];
-let uid = 1;
-
-function Book(uid, title, author, pubYear, numPages, status) {
-	this.uid = uid;
-	this.title = title;
-	this.author = author;
-	this.pubYear = pubYear;
-	this.status = status;
-	this.numPages = numPages;
-	this.info =
-		title +
-		" by " +
-		author +
-		", " +
-		pubYear +
-		". " +
-		numPages +
-		" pages." +
-		status;
-}
-
-function addBookToLibrary() {
-	let initialTitle = title.value;
-	let initialAuthor = author.value;
-	let initialPubYear = pubYear.value;
-	let initialStatus = status.value;
-
-	console.log(initialTitle);
-	console.log(initialAuthor);
-	console.log(initialPubYear);
-	console.log(initialStatus);
-
-	// Check if inputs is empty
-	if (
-		initialTitle === "" ||
-		initialAuthor === "" ||
-		initialPubYear === "" ||
-		initialStatus === ""
-	) {
-		console.log("nope");
-	} else {
-		let book = new Book(
-			uid,
-			initialTitle,
-			initialAuthor,
-			initialPubYear,
-			initialStatus
-		);
-		myLibrary.push(book);
-		displayBook(book);
-		modalBG.classList.remove("bg-active");
-		uid++;
-
-		// Clears input fields
-		title.value = "";
-		author.value = "";
-		pubYear.value = "";
-		status.value = "";
+// Book Class: Represents a Book
+class Book {
+	constructor(title, author, isbn) {
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
 	}
 }
 
-function displayBook(book) {
-	bookList.insertAdjacentHTML(
-		"beforeend",
-		`<div class="book-card">
-		 <p>Title:</p>
-		 <h2>${book.title}</h2>
-		 <p>Author:</p>
-		 <h4>${book.author}</h4>
-		 <p>Status:</p>
-		 <h4>${book.numPages} pages</h4>
-		 <div class="card-footer">
-		 	<button class="change-status">Change Status</button>
-		 	<button class="remove-book">Remove</button>
-		 </div></div>`
-	);
+// UI Class: Handle UI tasks
+class UI {
+	static displayBooks() {
+		const StoredBooks = Store.getBooks();
+
+		// Loop over StoredBooks then call AddBookToList
+		StoredBooks.forEach((book) => {
+			UI.addBookToList(book);
+		});
+	}
+
+	static addBookToList(book) {
+		const list = document.querySelector("#book-list");
+
+		const row = document.createElement("div");
+		row.className = "book-card";
+
+		row.innerHTML = `			<p>Title:</p>
+			<h2>${book.title}</h2>
+			<p>Author:</p>
+			<h4>${book.author}</h4>
+			<p>Status:</p>
+			<h4>${book.isbn}</h4>
+			<div class="card-footer">
+				<button class="change-status">Change Status</button>
+				<button class="delete-book">Remove</button>
+	 		</div>
+    `;
+
+		list.appendChild(row);
+	}
+
+	static deleteBook(el) {
+		if (el.classList.contains("delete-book")) {
+			el.parentElement.parentElement.remove();
+		}
+	}
+
+	// static showAlertMessage(message, className) {
+	// 	const div = document.createElement("div");
+	// 	div.className = `alert alert-${className}`;
+	// 	div.appendChild(document.createTextNode(message));
+
+	// 	const container = document.querySelector(".container");
+	// 	const form = document.querySelector("#book-form");
+	// 	container.insertBefore(div, form);
+
+	// 	// Alert vanishes in 3 seconds
+	// 	setTimeout(() => {
+	// 		document.querySelector(".alert").remove();
+	// 	}, 3000);
+	// }
+
+	static clearFields() {
+		document.querySelector("#title").value = "";
+		document.querySelector("#author").value = "";
+		document.querySelector("#isbn").value = "";
+	}
 }
 
-// Modal
+// Store Class: Handles Storage (Local)
+class Store {
+	static getBooks() {
+		let books;
+		if (localStorage.getItem("books") === null) {
+			books = [];
+		} else {
+			books = JSON.parse(localStorage.getItem("books"));
+		}
+
+		return books;
+	}
+
+	static addBook(book) {
+		const books = Store.getBooks();
+
+		books.push(book);
+
+		localStorage.setItem("books", JSON.stringify(books));
+	}
+
+	static removeBook(isbn) {
+		const books = Store.getBooks();
+		books.forEach((book, index) => {
+			if (book.isbn === isbn) {
+				books.splice(index, 1);
+			}
+		});
+
+		localStorage.setItem("books", JSON.stringify(books));
+	}
+}
+
+//Event: Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+// Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+	// Prevent actual submit
+	e.preventDefault();
+
+	// Get form values
+	const title = document.querySelector("#title").value;
+	const author = document.querySelector("#author").value;
+	const isbn = document.querySelector("#isbn").value;
+
+	// Validate
+	if (title === "" || author === "" || isbn === "") {
+		UI.showAlertMessage("Please fill in all fields", "danger");
+	} else {
+		// Instatiate book
+		const book = new Book(title, author, isbn);
+		// console.log(book);
+
+		// Add Book to UI
+		UI.addBookToList(book);
+
+		// Add book to storage
+		Store.addBook(book);
+
+		// Show success meassge
+		// UI.showAlertMessage("Book Added", "success");
+
+		// Close modal
+		modalBG.classList.remove("bg-active");
+
+		// Clear Fields
+		UI.clearFields();
+	}
+});
+
+// Event: Remove a Book
+document.querySelector(".book-list").addEventListener("click", (e) => {
+	// Remove book from UI
+	UI.deleteBook(e.target);
+
+	// Remove book form storage
+	Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+	// Show success meassge
+	// UI.showAlertMessage("Book Removed", "success");
+});
+
+// Modal Events
 const modalbtn = document.querySelector(".add-book");
 const modalBG = document.querySelector(".modal-bg");
 const modalClose = document.querySelector(".modal-close");
@@ -95,12 +161,4 @@ modalbtn.addEventListener("click", () => {
 
 modalClose.addEventListener("click", () => {
 	modalBG.classList.remove("bg-active");
-});
-
-addBook.addEventListener("click", addBookToLibrary);
-
-const removeBook = document.getElementById("remove-book");
-removeBook.addEventListener("click", () => {
-	console.log("remove");
-	removeBook.closest("div.book-card").remove();
 });
